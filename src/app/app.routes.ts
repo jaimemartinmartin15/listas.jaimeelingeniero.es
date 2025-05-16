960ba5f0208e698ceb673aaa29870171035ce9ff
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-05-16 19:34:06 +0200

    Generate my-lists component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MyListsComponent } from '../my-lists/my-lists.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'mis-listas',
+    component: MyListsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'mis-listas',
+  },
+];
