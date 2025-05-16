efbf76a7a5672fd30115a704c6046b341e09df27
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-05-16 19:48:13 +0200

    Update lists service and print list names.

--- src/my-lists/my-lists.component.ts
+++ src/my-lists/my-lists.component.ts
@@ -1,15 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ListsService } from '../services/Lists.service';
 
 @Component({
   selector: 'app-my-lists',
+  imports: [CommonModule],
   templateUrl: './my-lists.component.html',
   styleUrls: ['./my-lists.component.scss']
 })
 export class MyListsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public readonly listsService: ListsService) {}
 
-  ngOnInit() {
+  public ngOnInit() {
+
   }
 
 }
--- src/services/Lists.service.ts
+++ src/services/Lists.service.ts
@@ -5,15 +5,25 @@ import { List } from "../models/list";
   providedIn: 'root',
 })
 export class ListsService {
+  public allLists() {
+    return JSON.parse(localStorage.getItem('lists') || '[]');
+  }
+
   public createList(name: string) {
-    localStorage.setItem(name, JSON.stringify([]));
+    const existingLists: List[] = JSON.parse(localStorage.getItem('lists') || '[]');
+    existingLists.push({name, sections: []});
+    localStorage.setItem('lists', JSON.stringify(existingLists));
   }
 
   public updateList(list: List) {
-    localStorage.setItem(list.name, JSON.stringify(list));
+    const existingLists: List[] = JSON.parse(localStorage.getItem('lists') || '[]');
+    const el: List = existingLists.find(l => l.name === list.name)!;
+    existingLists[existingLists.indexOf(el)] = list;
+    localStorage.setItem('lists', JSON.stringify(existingLists));
   }
 
   public deleteList(name: string) {
-    localStorage.removeItem(name);
+    const existingLists: List[] = JSON.parse(localStorage.getItem('lists') || '[]');
+    localStorage.setItem('lists', JSON.stringify(existingLists.filter(l => l.name !== name)));
   }
 }
