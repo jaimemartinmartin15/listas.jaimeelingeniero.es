f0ba1744a95aca49767fe00478ab018f76c76729
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-06-07 14:49:38 +0200

    Implement list details component

--- src/list-details/list-details.component.ts
+++ src/list-details/list-details.component.ts
@@ -1,18 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { List } from '../models/list';
+import { ToPathPipe } from '../pipes/to-path.pipe';
 import { ListsService } from '../services/Lists.service';
+import { IconsSvgModule } from '../svg-output/icons-svg.module';
+import { ListSectionComponent } from './list-section/list-section.component';
 
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
   styleUrls: ['./list-details.component.scss'],
-  imports: [],
+  imports: [CommonModule, ListSectionComponent, IconsSvgModule],
 })
 export class ListDetailsComponent implements OnInit {
   @Input() listId!: string;
 
-  constructor(private readonly listsService: ListsService) { }
+  public list: List;
 
-  ngOnInit() {
-    console.log(this.listId);
+  constructor(private readonly listsService: ListsService, private readonly router: Router) { }
+
+  public ngOnInit() {
+    const toPathPipe = new ToPathPipe();
+    this.list = this.listsService.allLists().find(l => toPathPipe.transform(l.name) === this.listId)!;
+
+    if (!this.list) {
+      this.router.navigate(['']);
+      return;
+    }
   }
 }
