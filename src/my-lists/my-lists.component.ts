0ecdc959515ba706a6c1cda5305763a1d6b77f8e
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-06-09 15:53:58 +0200

    Redesign lists page.

--- src/my-lists/my-lists.component.ts
+++ src/my-lists/my-lists.component.ts
@@ -1,15 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { List } from '../models/list';
 import { ToPathPipe } from '../pipes/to-path.pipe';
 import { ListsService } from '../services/Lists.service';
+import { IconsSvgModule } from '../svg-output/icons-svg.module';
 
 @Component({
   selector: 'app-my-lists',
   templateUrl: './my-lists.component.html',
   styleUrls: ['./my-lists.component.scss'],
-  imports: [CommonModule, RouterLink, ToPathPipe],
+  imports: [CommonModule, RouterLink, ToPathPipe, IconsSvgModule],
 })
 export class MyListsComponent {
-  public constructor(public readonly listsService: ListsService) {}
+  public constructor(public readonly listsService: ListsService) { }
+
+  public completedItems(list: List) {
+    return list.sections.flatMap(section => section.items).filter(item => item.completed).length;
+  }
+
+  public totalItems(list: List) {
+    return list.sections.flatMap(section => section.items).length;
+  }
 }
